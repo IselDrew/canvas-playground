1244d6df5382bf97cf69e6cc2afd7acff96e10bd
Anton Slesariev <sonarct@gmail.com>
2018-09-23 13:38:14 +0300

    Wrap code with module

--- script.js
+++ script.js
@@ -1,87 +1,86 @@
-let canvas
-let ctx
+function init () {
+  let canvas
+  let ctx
 
-const speed = 10
+  const speed = 10
 
-const directions = {
-  38: 'up',
-  40: 'down',
-  37: 'left',
-  39: 'right'
-}
-
-const position = {
-  x: 150,
-  y: 100
-}
-
-const moving = {
-  up: false,
-  right: false,
-  down: false,
-  left: false
-}
-
-function draw() {
-  ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
-  move()
-
-  ctx.beginPath()
-  ctx.arc(position.x, position.y, 10, 0, 2 * Math.PI)
-  ctx.fill()
-
-  requestAnimationFrame(draw)
-}
-
-
-function resizeCanvas() {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-}
-
-function onLoadComplete() {
-  canvas = document.createElement('canvas')
-  document.body.appendChild(canvas)
-  ctx = canvas.getContext("2d")
-  resizeCanvas()
-  draw()
-}
-
-
-function keyWasPressed(event) {
-  const direction = directions[event.keyCode]
-  if (direction) {
-    moving[direction] = true
+  const directions = {
+    38: 'up',
+    40: 'down',
+    37: 'left',
+    39: 'right'
   }
+
+  const position = {
+    x: 150,
+    y: 100
+  }
+
+  const moving = {
+    up: false,
+    right: false,
+    down: false,
+    left: false
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
+    move()
+
+    ctx.beginPath()
+    ctx.arc(position.x, position.y, 10, 0, 2 * Math.PI)
+    ctx.fill()
+
+    requestAnimationFrame(draw)
+  }
+
+  function createCanvas() {
+    canvas = document.createElement('canvas')
+    document.body.appendChild(canvas)
+    ctx = canvas.getContext("2d")
+  }
+
+  function resizeCanvas() {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+  }
+
+  function onLoadComplete() {
+    createCanvas()
+    resizeCanvas()
+    draw()
+  }
+
+  function setDirection(keyCode, value) {
+    const direction = directions[keyCode]
+    if (direction) {
+      moving[direction] = value
+    }
+  }
+
+  function move() {
+    if (moving.up) {
+      position.y -= speed
+    }
+    if (moving.down) {
+      position.y += speed
+    }
+    if (moving.left) {
+      position.x -= speed
+    }
+    if (moving.right) {
+      position.x += speed
+    }
+  }
+
+  function onResize() {
+    resizeCanvas()
+  }
+
+  window.addEventListener('keydown', (event) => setDirection(event.keyCode, true))
+  window.addEventListener('keyup', (event) => setDirection(event.keyCode, false))
+  window.addEventListener('load', onLoadComplete)
+  window.addEventListener('resize', onResize)
 }
 
-function keyWasUnpressed(event) {
-  const direction = directions[event.keyCode]
-  if (direction) {
-    moving[direction] = false
-  }
-}
-
-function move() {
-  if (moving.up) {
-    position.y -= speed
-  }
-  if (moving.down) {
-    position.y += speed
-  }
-  if (moving.left) {
-    position.x -= speed
-  }
-  if (moving.right) {
-    position.x += speed
-  }
-}
-
-function onResize() {
-  resizeCanvas()
-}
-
-window.addEventListener('keydown', keyWasPressed)
-window.addEventListener('keyup', keyWasUnpressed)
-window.addEventListener('load', onLoadComplete)
-window.addEventListener('resize', onResize)
+init()
