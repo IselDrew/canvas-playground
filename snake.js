6a94b3a375727e0363642e8f6032bde7cbca3506
IselDrew <andryshonochek0604@gmail.com>
2019-01-30 17:23:04 +0200

    fix direction bug

--- snake.js
+++ snake.js
@@ -1,16 +1,25 @@
-const gamespeed = 100;
+const gamespeed = 500;
 const gameTimer = setInterval(draw, gamespeed);
 
 let canvas;
 let ctx; 
 
 const tile = 32;
-const mapSize = [22, 16];
+const mapSize = [5, 5];
 const mapWidth = mapSize[0] * tile;
 const mapHeight = mapSize[1] * tile;
+
+let score = 0;
 const scoreSizeZone = 5;
 
-let snake = [];
+let isGameOver = false;
+
+let snake = [generateCoordinates()];
+let head;
+let berry = generateCoordinates();
+
+let currentDirection;
+let direction;
 
 const directions = {
     right : "RIGHT",
@@ -26,6 +35,7 @@ const keys = {
     down : 38
 }
 
+
 function generateCoordinate (limit) {
     return Math.floor(Math.random() * limit) * tile;
 }
@@ -37,101 +47,124 @@ function generateCoordinates () {
     };
 }
 
-snake[0] = generateCoordinates();
-
-let berry = generateCoordinates();
-
-
-function draw() {
+function render() {
     ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, mapWidth  + (tile * scoreSizeZone), mapHeight);
-
-    tileMap();
     
+    //map
+    let snakeMapWidth = 1 + mapWidth/tile;
+    let snakeMapHeight = 1 + mapHeight/tile;
+    for (let i = 0; i < snakeMapWidth; i++) {
+        ctx.beginPath();
+        ctx.moveTo(i * tile, 0.5); //0.5
+        ctx.lineTo(i * tile, mapHeight + 0.5);
+          ctx.stroke();
+    }   
+    for (let i = 0; i < snakeMapHeight; i++) {
+        ctx.beginPath();
+        ctx.moveTo(0, i * tile + 0.5);
+        ctx.lineTo(mapWidth, i * tile + 0.5);
+        ctx.stroke();
+    } 
+
     ctx.beginPath();
 
-    let score = (snake.length - 1) * 10;
- 
-    menu(score);
+    //berry
+    ctx.fillStyle = 'red';
+    ctx.fillRect(berry.x, berry.y, tile, tile);
     
+    //snake body
     for (let i = 1; i < snake.length; i++) {
         ctx.fillStyle = 'green';
         ctx.fillRect(snake[i].x, snake[i].y, tile, tile);
     }
 
+    //snake head
     ctx.fillStyle = 'black';
     ctx.fillRect(snake[0].x, snake[0].y, tile, tile);
 
-    ctx.fillStyle = 'red';
-    ctx.fillRect(berry.x, berry.y, tile, tile);
-
-    let newCoords = {
-        x : snake[0].x,
-        y : snake[0].y 
-    }
-
-    newCoords = move(newCoords.x, newCoords.y);
-    
-    let head = {
-        x : newCoords.x,
-        y : newCoords.y
-    };  
-
-    let checkBerry = berry; 
-   
-    if (head.x === berry.x && head.y === berry.y) {
-         checkBerry = generateCoordinates();  
-    } else { 
-        snake.pop();
-    }
-
-    berryValidation(checkBerry); //Johnny B. Goode
-   
-    if (snakeCollision(head, snake)) {
-        gameOver();
-    };
-
-    snake.unshift(head);
-
-    if (bordersCollision(snake)) {
-        gameOver();
-    };
-}
-
-
-//----------------------------Menu parts-------------------------
-function menu(score) {
+    //score
     ctx.font = '30px times new roman';
     ctx.strokeText("Your Score:", mapWidth + 10, 50);
     ctx.font = '40px times new roman';
     let scoreSize = ctx.measureText(score);
     ctx.strokeText(score, (mapWidth + tile * 4.5) - scoreSize.width, tile * 3);
+
+    //GameOver
+    if (isGameOver) { 
+        ctx.font = '75px times new roman';
+        ctx.fillStyle = 'red';
+        ctx.fillText("Game Over", mapWidth / 3, mapHeight / 2.2);
+    }
+
+    requestAnimationFrame(render);
 }
 
-function gameOver() {
-//  console.log(true);
-    clearInterval(gameTimer);
+function draw() {
+    score = (snake.length - 1) * 10;
 
-    ctx.font = '75px times new roman';
-    ctx.fillStyle = 'red';
-    ctx.fillText("Game Over", mapWidth / 3, mapHeight / 2.2);
+    if (!currentDirection) {
+        currentDirection = direction;
+    }
+
+    const newCoords = move(snake[0].x, snake[0].y);
+    
+    currentDirection = direction;
+
+    head = {
+        x : newCoords.x,
+        y : newCoords.y
+    };  
+    
+    let checkBerry = berry; 
+    
+    console.log("Before pop:", snake.length); 
+
+    if (head.x === berry.x && head.y === berry.y) {
+         checkBerry = generateCoordinates();  
+    } else { 
+        snake.pop();
+    }
+   
+    console.log("After pop:", snake.length); 
+
+    while (!isBerryCoordsValid(snake, checkBerry)) {
+        console.log("Alert, regenerating coords")
+        checkBerry = generateCoordinates();
+    }
+
+    berry = checkBerry;
+
+    if (snakeCollision(head, snake)) {
+        gameOver();
+    }
+
+    snake.unshift(head);
+
+    if (bordersCollision(head)) {
+        gameOver();
+    }
+}
+
+
+//----------------------------Menu parts-------------------------
+function gameOver() {
+    clearInterval(gameTimer);
+    isGameOver = true;
 }
 
 //--------------------------Movement---------------------------
-let direction;
-
 function keyWasPressed(event) {
     let key = event.keyCode;
-    if ( key === keys.right && direction !== directions.right) {
+    if ( key === keys.right && currentDirection !== directions.right) {
         direction = directions.left;
-    } else if (key === keys.down && direction !== directions.down) {
+    } else if (key === keys.down && currentDirection !== directions.down) {
         direction = directions.up;
-    } else if (key === keys.left && direction !== directions.left) {
+    } else if (key === keys.left && currentDirection !== directions.left) {
         direction = directions.right;
-    } else if (key === keys.up && direction !== directions.up) {
+    } else if (key === keys.up && currentDirection !== directions.up) {
         direction = directions.down;
     } 
-   // console.log(direction)
 }
 
 function move(x, y) {
@@ -154,9 +187,7 @@ function move(x, y) {
 //-------------------------Map Logic---------------------------
 
 function snakeCollision(objHead,arrSnake) {
-    for (let i = 0; i < arrSnake.length - 1; i++) {
-        // console.log("Head pos: ", tail.x / tile, tail.y / tile)
-        // console.log("Elem pos:", snake[i].x / tile, snake[i].y / tile)
+    for (let i = 0; i < arrSnake.length; i++) {
         if (objHead.x === arrSnake[i].x && objHead.y === arrSnake[i].y) {
             return true;
         }
@@ -164,42 +195,25 @@ function snakeCollision(objHead,arrSnake) {
     return false;
 }
 
-function bordersCollision(array) {
-    if (array[0].x < 0 || array[0].x >= mapWidth || array[0].y < 0 || array[0].y >= mapHeight) {
+function bordersCollision(head) {
+    //if (array[0].x < 0 || array[0].x >= mapWidth || array[0].y < 0 || array[0].y >= mapHeight) {
+    if (head.x < 0 || head.x >= mapWidth || head.y < 0 || head.y >= mapHeight) {
         return true;
     }
 }
 
-function berryValidation(berryCoords) {
+function isBerryCoordsValid(snake, berry) {
     for (let i = 0; i < snake.length; i++) {
-        if (snake[i].x === berryCoords.x && snake[i].y === berryCoords.y) {
-            console.log("ALERT! Regenerating berry coord's");
-            berryCoords = generateCoordinates();
-        } else {
-            berry = berryCoords;
+        if (snake[i].x === berry.x && snake[i].y === berry.y) {
+            return false;
         }
-    } 
+    }
+    return true;
 }
 
 
-function tileMap() {
-    let snakeMapWidth = 1 + mapWidth/tile;
-    let snakeMapHeight = 1 + mapHeight/tile;
-     for (let i = 0; i < snakeMapWidth; i++) {
-      ctx.beginPath();
-      ctx.moveTo(i * tile, 0.5); //0.5
-      ctx.lineTo(i * tile, mapHeight + 0.5);
-      ctx.stroke();
-    }   
-    for (let i = 0; i < snakeMapHeight; i++) {
-      ctx.beginPath();
-      ctx.moveTo(0, i * tile + 0.5);
-      ctx.lineTo(mapWidth, i * tile + 0.5);
-      ctx.stroke();
-    } 
-}
 
-
+//--------------------------Other--------------------------
 function resizeCanvas() {
     canvas.width = mapWidth + (tile * scoreSizeZone);
     canvas.height = mapHeight;
@@ -209,7 +223,7 @@ function onLoadComplete() {
     canvas = document.createElement('canvas');
     document.body.appendChild(canvas);
     resizeCanvas(); //1. put size of canvas window
-    draw(); //2. draw objects
+    requestAnimationFrame(render); //2. draw objects
 }
 
 //---------------------EventListeners----------------------
